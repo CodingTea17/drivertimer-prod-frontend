9687f8ad3d25c06591700da59789d21a80e10aca
Dawson M <dawson.mortenson@gmail.com>
2018-01-07 11:48:36 -0800

    add a map component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Store from './Store.js';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class Home extends Component {
   constructor() {
     super();
     this.state = {
       stores: [],
+      center: {lat: 59.95, lng: 30.33},
+      zoom: 11
     };
   }
 
@@ -20,6 +25,16 @@ class Home extends Component {
   render() {
     return (
       <div>
+        <GoogleMapReact
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text={'Kreyser Avrora'}
+          />
+        </GoogleMapReact>
         {
           this.state.stores.map((store, index) => (
             <Store
