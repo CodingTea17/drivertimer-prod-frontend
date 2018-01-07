465472d29e197ffe7d8206d384227314bbfab48b
Dawson M <dawson.mortenson@gmail.com>
2018-01-07 12:32:26 -0800

    define process.env

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -251,6 +251,11 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      }
+    })
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
