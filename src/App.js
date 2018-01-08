9bb0b1fcf871ff9a2ade9d456ff63258d6240de4
Dawson M <dawson.mortenson@gmail.com>
2018-01-07 16:17:57 -0800

    sound manager witch hunt

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
+import 'soundmanager2';
+
+// Setup soundManager
+soundManager.setup({ ignoreMobileRestrictions: true });
 
 const store = configureStore();
 
